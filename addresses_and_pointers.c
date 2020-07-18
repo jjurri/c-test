174dd4eb0d463ded09a4b2de86649ac46418f7c1
iurii <iuriirybin27@gmail.com>
2020-07-18 14:38:02 +0300

    addresses_and_pointers

--- addresses_and_pointers.c
+++ addresses_and_pointers.c
@@ -4,9 +4,17 @@ int main(int argc, char* argv[])
 {
     int i = 10;
     int *pi = &i;
-    int *pi = &pi;
-    int *pppi = &ppi;
+    int **ppi = &pi;
+    int ***pppi = &ppi;
+
     printf("%d\n", i);
+    *pi = 20;
+    printf("%d\n", i);
+    **ppi = 30;
+    printf("%d\n", i);
+    ***pppi = 40;
+    printf("%d\n", i);
+
 
     return 0;
 }
--- typecasting.c
+++ typecasting.c
@@ -5,12 +5,12 @@ int main(int argc, char* argv[])
     // int неявно приводится к unsigned int
     int x  = -100;
     unsigned int y = 10U;
-    long long int z = x + y;
-    printf("z =%lld\n", z);
+    long long int z = -100 + 10U;
+    printf("z = %lld\n", z);
 
     // char явно приводится к типу int
     char c ='Я';
-    int d = (int)c * 10 + y;
+    int d = (int)c * 10;
     printf("d = %d\n", d);
 
     return 0;
