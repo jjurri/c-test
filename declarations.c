a04f4ee9449e4d85848bfe5e59338115d2b766e1
iurii <iuriirybin27@gmail.com>
2020-07-18 12:12:50 +0300

    inc inttypes

--- declarations.c
+++ declarations.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <inttypes.h>
 
 int main(int argc, char* argv[])
 {
-    char c;
-    int i;
-    double d;
-    bool b;
+    unsigned char c;
+    int8_t i;
+    int16_t i;
+    int32_t i32;
+    int64_t i64;
 
     return 0;
 }
