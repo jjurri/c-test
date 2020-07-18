1be7b4efa6f27f5234f38cdf9a1fade2fe8758c0
iurii <iuriirybin27@gmail.com>
2020-07-18 12:36:24 +0300

    typecasting

---
+++ typecasting.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    // int неявно приводится к unsigned int
+    int x  = -100;
+    unsigned int y = 10U;
+    long long int z = x + y;
+    printf("z =%lld\n", z);
+
+    // char явно приводится к типу int
+    char c ='Я';
+    int d = (int)c * 10 + y;
+    printf("d = %d\n", d);
+
+    return 0;
+}
+
